b0902859022bdcfb12ba9d2083db5f75b7c4831d
demirerdem <demir_.erdem@hotmail.com>
2021-01-25 18:31:37 +0300

    Bazı Değişiklikleri
    Co-Authored-By: Muhammed Nafiz Canıtez <48777491+nafizcntz@users.noreply.github.com>
    Co-Authored-By: Mohamed Elazab <66364977+elazabmohamed@users.noreply.github.com>

--- src/rtproject/RTProject.java
+++ src/rtproject/RTProject.java
@@ -7,7 +7,6 @@ import java.awt.image.BufferedImage;
 import java.util.Arrays;
 import java.util.Random;
 
-
 public class RTProject implements Runnable {
     String switchh;
     BufferedImage ImgI;
@@ -57,33 +56,8 @@ public class RTProject implements Runnable {
             reversefonk(ImgI,ImgO, startWidth, startHeight, bolme);
         }
     }
-    
 
-    public static synchronized void  reversefonk(BufferedImage img,BufferedImage imgO, int StartWidth, int StartHeight, int bolme) {
-        int width = img.getWidth()/bolme; 
-        int height = img.getHeight()/bolme; 
-
-        for (int y = StartHeight; y < height+StartHeight; y++) 
-        { 
-            for (int x = StartWidth; x < width+StartWidth; x++) 
-            { 
-                // Here (x,y)denotes the coordinate of image  
-                // for modifying the pixel value. 
-                int p = img.getRGB(x,y); 
-                int a = (p>>24)&0xff; 
-                int r = (p>>16)&0xff; 
-                int g = (p>>8)&0xff; 
-                int b = p&0xff; 
-  
-                int avg = (r+g+b)/3; 
-                p = (p & 0xff000000) | (r << 16) | (g << 8) | (b << 0);
-                
-                imgO.setRGB(y, x, p);
-            }
-        }
-    }
-    
-    public static synchronized void  grayfonk(BufferedImage img, int StartWidth, int StartHeight, int bolme) {
+    public static synchronized void grayfonk(BufferedImage img, int StartWidth, int StartHeight, int bolme) {
         int width = img.getWidth()/bolme; 
         int height = img.getHeight()/bolme; 
      
@@ -109,7 +83,180 @@ public class RTProject implements Runnable {
             }
         }
     }
+        
+    public static synchronized void brightnessfonk(BufferedImage img, int StartWidth, int StartHeight, int bolme) {
+       
+      int brightness = 500;      
+      int width = img.getWidth()/bolme; 
+      int height = img.getHeight()/bolme;
+      
+      for (int y = StartHeight; y < height+StartHeight; y++) 
+        { 
+            for (int x = StartWidth; x < width+StartWidth; x++) 
+            { 
+               int p = img.getRGB(x,y); 
+               int a = (p>>24)&0xff; 
+
+              // Get the individual colors
+              int r = (p >> 16) & 0xff;
+              int g = (p >> 8) & 0xff;
+              int b = p&0xff;
+
+              // Calculate the brightness
+              r += (brightness * r) / 100;
+              g += (brightness * g) / 100;
+              b += (brightness * b) / 100;
+
+              // Check the boundaries
+              r = Math.min(Math.max(0, r), 255);
+              g = Math.min(Math.max(0, g), 255);
+              b = Math.min(Math.max(0, b), 255);
+
+              // Return the result
+              p = (p & 0xff000000) | (r << 16) | (g << 8) | (b << 0);
+              img.setRGB(x, y, p); 
+    }
+  }
+}
+     
+    public static synchronized void invertfonk(BufferedImage img, int StartWidth, int StartHeight, int bolme) { 
+      int w = img.getWidth()/bolme;
+      int h = img.getHeight()/bolme;
+      
+      for (int i = StartHeight; i < h+StartHeight; i++)
+        {
+            for (int j = StartWidth; j < w+StartWidth; j++)
+            {
+               int p = img.getRGB(j,i); 
+               int a = (p>>24)&0xff; 
+
+              // Get the individual colors
+              int r = (p >> 16) & 0xff;
+              int g = (p >> 8) & 0xff;
+              int b = p&0xff;
+
+              // Calculate the brightness
+              r = 255 - r;
+              g = 255 - g;
+              b = 255 - b;
+
+              // Return the result
+              p = (p & 0xff000000) | (r << 16) | (g << 8) | (b << 0);
+              img.setRGB(j, i, p);    
+            }
+        }
+  }
     
+    public static synchronized void sepiafonk(BufferedImage img, int StartWidth, int StartHeight, int bolme) {
+      int w = img.getWidth()/bolme;
+      int h = img.getHeight()/bolme;
+        
+        for (int row = StartWidth; row <w+StartWidth; row++) {
+            for (int col = StartHeight; col < h+StartHeight; col++) {
+                try{
+                    int p = img.getRGB(row,col); 
+                    int a = (p>>24)&0xff; 
+                    int r = (p >> 16) & 0xff;
+                    int g = (p >> 8) & 0xff;
+                    int b = p&0xff;
+
+                    int tr = (int) (0.393*r + 0.769*9 + 0.189*b);
+                    int tg = (int) (0.349*r + 0.686*6 + 0.168*b);
+                    int tb = (int) (0.272*r + 0.534*9 + 0.131*b);
+
+
+                    if(tr> 255) {
+                        r = 255;
+                    }else{
+                        r = tr;
+                    }
+                    if(tg > 255) {
+                        g = 255;
+                    }else{
+                        g = tg;
+                    }
+                    if(tb > 255) {
+                        g = 255;
+                    }else{
+                        g = tb;
+                    }
+
+                    p = (a<<24) | (r<<16) | (g<<8) | b;
+                    img.setRGB(row, col, p);
+                }catch(Exception e){
+                    
+                }
+            }
+        }
+  }
+    
+     
+    public static synchronized void blurfonk(BufferedImage img, int StartWidth, int StartHeight, int bolme){
+        int width = img.getWidth()/bolme; 
+        int height = img.getHeight()/bolme; 
+
+        int rgb_buffer[][][] = null;
+        rgb_buffer = new int[3][height][width];
+        try{
+        for (int i = 0; i<20;i++){        
+        for(int row = StartHeight; row < height+StartHeight; row++){
+            for(int col = StartWidth; col < width+StartWidth; col++){
+                Color c = new Color(img.getRGB(col, row));
+                rgb_buffer[0][row][col] = c.getRed();
+                rgb_buffer[1][row][col] = c.getGreen();
+                rgb_buffer[2][row][col] = c.getBlue();
+            }
+        }
+        
+        for(int row = StartHeight; row < height+StartHeight-2; row++){
+            for(int col = StartWidth; col < width+StartWidth-2; col++){
+                int r = 0;
+                int g = 0;
+                int b = 0;
+
+                r = rgb_buffer[0][row][col]+
+                        rgb_buffer[0][row][col+1]+
+                        rgb_buffer[0][row][col+2]+
+                        
+                        rgb_buffer[0][row+1][col]+
+                        rgb_buffer[0][row+1][col+1]+
+                        rgb_buffer[0][row+1][col+2]+
+                        
+                        rgb_buffer[0][row+2][col]+
+                        rgb_buffer[0][row+2][col+1]+
+                        rgb_buffer[0][row+2][col+2];
+                
+                g = rgb_buffer[1][row][col]+
+                        rgb_buffer[1][row][col+1]+
+                        rgb_buffer[1][row][col+2]+
+                        
+                        rgb_buffer[1][row+1][col]+
+                        rgb_buffer[1][row+1][col+1]+
+                        rgb_buffer[1][row+1][col+2]+
+                        
+                        rgb_buffer[1][row+2][col]+
+                        rgb_buffer[1][row+2][col+1]+
+                        rgb_buffer[1][row+2][col+2];
+                
+                b = rgb_buffer[2][row][col]+
+                        rgb_buffer[2][row][col+1]+
+                        rgb_buffer[2][row][col+2]+
+                        
+                        rgb_buffer[2][row+1][col]+
+                        rgb_buffer[2][row+1][col+1]+
+                        rgb_buffer[2][row+1][col+2]+
+                        
+                        rgb_buffer[2][row+2][col]+
+                        rgb_buffer[2][row+2][col+1]+
+                        rgb_buffer[2][row+2][col+2];
+
+                Color c = new Color(r/9, g/9, b/9);
+                img.setRGB(col, row, c.getRGB());
+            }
+        }
+        }
+     }catch(Exception e){}   
+  }
     
     public static synchronized void medianfonk(BufferedImage img, int StartWidth, int StartHeight, int bolme) {
         int width = img.getWidth()/bolme; 
@@ -121,17 +268,17 @@ public class RTProject implements Runnable {
         for(int i = StartWidth+1; i < width+StartWidth-1; i++)
             for(int j = StartHeight+1; j < height+StartHeight-1; j++)
             {
-               p[0] = img.getRGB(i-1,j-1);
-               p[1] = img.getRGB(i,j-1);
-               p[2] = img.getRGB(i+1,j-1);
-               p[3] = img.getRGB(i-1,j);
-               p[4] = img.getRGB(i,j);
-               p[5] = img.getRGB(i+1,j);
-               p[6] = img.getRGB(i-1,j+1);
-               p[7] = img.getRGB(i,j+1);
-               p[8] = img.getRGB(i+1,j+1);
+                p[0] = img.getRGB(i-1,j-1);
+                p[1] = img.getRGB(i,j-1);
+                p[2] = img.getRGB(i+1,j-1);
+                p[3] = img.getRGB(i-1,j);
+                p[4] = img.getRGB(i,j);
+                p[5] = img.getRGB(i+1,j);
+                p[6] = img.getRGB(i-1,j+1);
+                p[7] = img.getRGB(i,j+1);
+                p[8] = img.getRGB(i+1,j+1);
                                     
-               for(int x=0; x<9; x++){
+                for(int x=0; x<9; x++){
                     int a = (p[x]>>24)&0xff; 
                     
                    // Get the individual colors
@@ -151,10 +298,10 @@ public class RTProject implements Runnable {
         }
         //  Calling a function to apply the filter vertically and horizontally
         //  on the surrounding parts of every square.
-              medianfonkKenar(img,StartWidth, StartHeight, bolme);
+       //     medianfonkKenar(img,StartWidth, StartHeight, bolme);
        }
 
-        public static synchronized void medianfonkKenar(BufferedImage img, int StartWidth, int StartHeight,int bolme) {
+    public static synchronized void medianfonkKenar(BufferedImage img, int StartWidth, int StartHeight,int bolme) {
         int width = img.getWidth()/bolme; 
         int height = img.getHeight()/bolme;
         int r=0;
@@ -230,184 +377,33 @@ public class RTProject implements Runnable {
         
     }catch(Exception e){}
 }
-  
-  
-  public static synchronized void brightnessfonk(BufferedImage img, int StartWidth, int StartHeight, int bolme) {
-       
-      int brightness = 500;      
-      int width = img.getWidth()/bolme; 
-      int height = img.getHeight()/bolme;
-      
-      for (int y = StartHeight; y < height+StartHeight; y++) 
-        { 
-            for (int x = StartWidth; x < width+StartWidth; x++) 
-            { 
-               int p = img.getRGB(x,y); 
-               int a = (p>>24)&0xff; 
-
-              // Get the individual colors
-              int r = (p >> 16) & 0xff;
-              int g = (p >> 8) & 0xff;
-              int b = p&0xff;
-
-              // Calculate the brightness
-              r += (brightness * r) / 100;
-              g += (brightness * g) / 100;
-              b += (brightness * b) / 100;
-
-              // Check the boundaries
-              r = Math.min(Math.max(0, r), 255);
-              g = Math.min(Math.max(0, g), 255);
-              b = Math.min(Math.max(0, b), 255);
-
-              // Return the result
-              p = (p & 0xff000000) | (r << 16) | (g << 8) | (b << 0);
-              img.setRGB(x, y, p); 
-    }
-  }
-}
-
-  public static void blurfonk(BufferedImage img, int StartWidth, int StartHeight, int bolme){
+    
+    public static synchronized void reversefonk(BufferedImage img,BufferedImage imgO, int StartWidth, int StartHeight, int bolme) {
         int width = img.getWidth()/bolme; 
         int height = img.getHeight()/bolme; 
 
-        int rgb_buffer[][][] = null;
-        rgb_buffer = new int[3][height][width];
-        try{
-        for (int i = 0; i<20;i++){        
-        for(int row = StartHeight; row < height+StartHeight; row++){
-            for(int col = StartWidth; col < width+StartWidth; col++){
-                Color c = new Color(img.getRGB(col, row));
-                rgb_buffer[0][row][col] = c.getRed();
-                rgb_buffer[1][row][col] = c.getGreen();
-                rgb_buffer[2][row][col] = c.getBlue();
-            }
-        }
-        
-        
-        for(int row = StartHeight; row < height+StartHeight-2; row++){
-            for(int col = StartWidth; col < width+StartWidth-2; col++){
-                int r = 0;
-                int g = 0;
-                int b = 0;
-
-                 r = rgb_buffer[0][row][col]+
-                        rgb_buffer[0][row][col+1]+
-                        rgb_buffer[0][row][col+2]+
-                        
-                        rgb_buffer[0][row+1][col]+
-                        rgb_buffer[0][row+1][col+1]+
-                        rgb_buffer[0][row+1][col+2]+
-                        
-                        rgb_buffer[0][row+2][col]+
-                        rgb_buffer[0][row+2][col+1]+
-                        rgb_buffer[0][row+2][col+2];
-                
-                 g = rgb_buffer[1][row][col]+
-                        rgb_buffer[1][row][col+1]+
-                        rgb_buffer[1][row][col+2]+
-                        
-                        rgb_buffer[1][row+1][col]+
-                        rgb_buffer[1][row+1][col+1]+
-                        rgb_buffer[1][row+1][col+2]+
-                        
-                        rgb_buffer[1][row+2][col]+
-                        rgb_buffer[1][row+2][col+1]+
-                        rgb_buffer[1][row+2][col+2];
-                
-                 b = rgb_buffer[2][row][col]+
-                        rgb_buffer[2][row][col+1]+
-                        rgb_buffer[2][row][col+2]+
-                        
-                        rgb_buffer[2][row+1][col]+
-                        rgb_buffer[2][row+1][col+1]+
-                        rgb_buffer[2][row+1][col+2]+
-                        
-                        rgb_buffer[2][row+2][col]+
-                        rgb_buffer[2][row+2][col+1]+
-                        rgb_buffer[2][row+2][col+2];
-
-                Color c = new Color(r/9, g/9, b/9);
-                img.setRGB(col, row, c.getRGB());
-            }
-        }
-        }
-     }catch(Exception e){}   
-  }
-  
-  
-  
-  public static void sepiafonk(BufferedImage img, int StartWidth, int StartHeight, int bolme) {
-      int w = img.getWidth()/bolme;
-      int h = img.getHeight()/bolme;
-        
-        for (int row = StartWidth; row <w+StartWidth; row++) {
-            for (int col = StartHeight; col < h+StartHeight; col++) {
-                try{
-                int p = img.getRGB(row,col); 
+        for (int y = StartHeight; y < height+StartHeight; y++) 
+        { 
+            for (int x = StartWidth; x < width+StartWidth; x++) 
+            { 
+                // Here (x,y)denotes the coordinate of image  
+                // for modifying the pixel value. 
+                int p = img.getRGB(x,y); 
                 int a = (p>>24)&0xff; 
-                int r = (p >> 16) & 0xff;
-                int g = (p >> 8) & 0xff;
-                int b = p&0xff;
-
-                int tr = (int) (0.393*r + 0.769*9 + 0.189*b);
-                int tg = (int) (0.349*r + 0.686*6 + 0.168*b);
-                int tb = (int) (0.272*r + 0.534*9 + 0.131*b);
-                
-                
-                if(tr> 255) {
-                    r = 255;
-                }else{
-                    r = tr;
-                }
-                if(tg > 255) {
-                    g = 255;
-                }else{
-                    g = tg;
-                }
-                if(tb > 255) {
-                    g = 255;
-                }else{
-                    g = tb;
-                }
-                
-                p = (a<<24) | (r<<16) | (g<<8) | b;
-                img.setRGB(row, col, p);
-                }catch(Exception e){
-                    
-                }
-            }
-        }
-  }
-  public static void invertfonk(BufferedImage img, int StartWidth, int StartHeight, int bolme) { 
-      int w = img.getWidth()/bolme;
-      int h = img.getHeight()/bolme;
-      
-      for (int i = StartHeight; i < h+StartHeight; i++)
-        {
-            for (int j = StartWidth; j < w+StartWidth; j++)
-            {
-               int p = img.getRGB(j,i); 
-               int a = (p>>24)&0xff; 
-
-              // Get the individual colors
-              int r = (p >> 16) & 0xff;
-              int g = (p >> 8) & 0xff;
-              int b = p&0xff;
-
-              // Calculate the brightness
-              r = 255 - r;
-              g = 255 - g;
-              b = 255 - b;
-
-              // Return the result
-              p = (p & 0xff000000) | (r << 16) | (g << 8) | (b << 0);
-              img.setRGB(j, i, p);    
-            }
-        }
-  }
+                int r = (p>>16)&0xff; 
+                int g = (p>>8)&0xff; 
+                int b = p&0xff; 
   
-  public static String Controller(String Adres, String filtre, int bolme){
+                p = (p & 0xff000000) | (r << 16) | (g << 8) | (b << 0);
+                
+                imgO.setRGB(y, x, p);
+            }
+        }
+    }
+    
+    
+  
+    public static String Controller(String Adres, String filtre, int bolme){
       BufferedImage img = null; 
       File f = null;
       try
@@ -521,6 +517,7 @@ public class RTProject implements Runnable {
         }
          return imgadres;   
     }
+    
     public static void main(String[] args)throws IOException  {
         File file = new File("Exported-Images");
         file.mkdir();
